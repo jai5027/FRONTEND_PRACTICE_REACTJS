c875f2f77ec80231ba76f3e2232314d56ec0c512
Jai Sharma <jaisharma27868@gmail.com>
2026-04-19 23:55:45 +0530

    resultgrid

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -4,28 +4,47 @@ import { setQuery, setResults, setError, setLoading} from '../redux/features/sea
 import { useEffect } from 'react'
 
 const ResultGrid = () => {
-
+    const dispatch = useDispatch()
     const {query, activeTab, results, loading, error} = useSelector((store) => store.search)
     
     useEffect(() => {
         if(!query) return 
+  const getData = async () => { 
 
-      const getData = async () => { 
-        let data
+        try {
+        let data = []
         if(activeTab == 'Photos'){
             let response = await fetchPhoto(query)
-            data = response.results
+            data = response.results.map((item) => ({
+                id: item.id,
+                type: 'photo',
+                title: item.alt_description,
+                thumbnail: item.urls.small,
+                src: item.urls.full
+            }))
         }
 
         if(activeTab == 'Videos'){
             let response = await fetchVideos(query)
-            data = response.videos
+            data = response.videos.map((item) => ({
+                id: item.id,
+                type: 'video',
+                title: item.user.name || 'video',
+                thumbnail: item.image,
+                src: item.video_files[0].link              
+            }))
         }
-    console.log(data)
-
-    }
+      dispatch(setResults(data))
+    
+        } catch (error) {
+          dispatch(setError(error.message))
+        }
+      }
     getData()
     }, [activeTab, query])
+
+    if(error) return <h1>Error</h1>
+    if(loading) return <h1>Loading....</h1>
 
   return (
     <div>
