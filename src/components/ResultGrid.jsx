5c4d192a022a29837e94cf9734adfc489d24bc2e
Jai Sharma <jaisharma27868@gmail.com>
2026-04-19 20:41:00 +0530

    tabs

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPhoto, fetchVideos } from '../api/mediaApi.js'
+import { setQuery, setResults, setError, setLoading} from '../redux/features/searchSlice.js'
+import { useEffect } from 'react'
 
 const ResultGrid = () => {
+
+    const {query, activeTab, results, loading, error} = useSelector((store) => store.search)
+    
+    useEffect(() => {
+        if(!query) return 
+
+      const getData = async () => { 
+        let data
+        if(activeTab == 'Photos'){
+            let response = await fetchPhoto(query)
+            data = response.results
+        }
+
+        if(activeTab == 'Videos'){
+            let response = await fetchVideos(query)
+            data = response.videos
+        }
+    console.log(data)
+
+    }
+    getData()
+    }, [activeTab, query])
+
   return (
     <div>
       
--- src/redux/features/searchSlice.js
+++ src/redux/features/searchSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
       query: '',
-      activeTab: 'photos',
+      activeTab: 'Photos',
       results: [],
       loading: false,
       error: null
