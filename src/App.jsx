5d2b36300e1b3a193f8293f3e1b9e7bd14543c6d
Jai Sharma <jaisharma27868@gmail.com>
2026-04-19 11:28:09 +0530

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import Search from "./Components/Search.jsx"
+import { fetchPhoto } from './api/mediaApi.js'
+import { fetchVideos } from './api/mediaApi.js';
 
 function App() {
 
 
   return (
     <>
- <Search />
+ <div className='h-screen text-white bg-gray-950'>
+  <button onClick={async () =>{ await fetchPhoto('cat')}}
+   className='bg-sky-500 px-4 py-2 m-5 rounded-sm cursor-pointer hover:bg-sky-600'  
+  >GET PHOTO</button>
+
+  <button onClick={async () =>{ await fetchVideos('cat')}}
+   className='bg-sky-500 px-4 py-2 m-5 rounded-sm cursor-pointer hover:bg-sky-600'    
+  >
+GET VIDEOS
+  </button>
+  </div>
     </>
   )
 }
