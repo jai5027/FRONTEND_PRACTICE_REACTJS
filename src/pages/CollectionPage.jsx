0adc767e951fce27cac9bd70270cca1a46d3a112
Jai Sharma <jaisharma27868@gmail.com>
2026-04-23 23:14:58 +0530

    clear button

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,17 +1,24 @@
 import { useSelector } from "react-redux"
 import CollectionCard from "../components/CollectionCard"
 
+
 const CollectionPage = () => {
 
   const collection = useSelector(state => state.collection.items)
 
   return (
+    <div className="px-20 ">
+      <div className="flex justify-between mb-10">
+      <h1 className="text-xl font-bold tracking-wide">YOUR COLLECTION</h1>
+      <button className="bg-red-500 px-2 py-2 rounded">Clear All</button>
+      </div>
     <div className='flex flex-wrap justify-center gap-5 w-full overflow-auto mx-auto'>
       {collection.map((items, index) => {
         return <div key={index}>
           <CollectionCard item={items} />
         </div>
       })}
+    </div>
     </div>
   )
 }
